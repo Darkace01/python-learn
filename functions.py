781c3b995eb18305212c60bbcb87c2441519d2b4
Kazeem Quadri <quadrikazeem01@gmail.com>
2025-01-22 16:18:24 +0000

    Add functions for cube calculation and conditional statements

--- functions.py
+++ functions.py
@@ -10,3 +10,46 @@ def say_hi(name, age):
     print('Hello ' + name + ', you are ' + age)
 
 say_hi('Mike', "35")
+
+
+# Return Statement
+
+def cube(num):
+    return num*num*num
+
+result = cube(4)
+print(result)
+
+
+# If Statements
+
+is_male = True
+is_tall = True
+
+if is_male or is_tall:
+    print('You are a male or tall or both')
+else:
+    print('You are neither male or tall')
+
+
+if is_male and is_tall:
+    print('You are a tall male')
+elif is_male and not(is_tall):
+    print('You are a short male')
+
+elif not(is_male) and is_tall:
+    print('You are not a male but are tall')
+else:
+    print('You are either mot male or tall or both')
+
+
+# If Statements & Comparisons
+def max_num(num1,num2,num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+    
+print(max_num(3,4,5))
