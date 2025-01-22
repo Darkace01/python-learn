513df51803176f156751c256ef79c5358ef90563
Kazeem Quadri <quadrikazeem01@gmail.com>
2025-01-22 14:41:48 +0000

    Enhance list operations: add pop, index, count, sort, reverse, and copy methods

--- list.py
+++ list.py
@@ -6,3 +6,17 @@ print(friends[1:3])
 print(friends[1])
 friends[1] = 'Mike'
 print(friends[1])
+
+friends.pop()
+
+print(friends.index('Sally'))
+print(friends.count('Sally'))
+
+friends.sort()
+print(friends)
+
+friends.reverse()
+print(friends)
+
+friends2 = friends.copy()
+print(friends2)
