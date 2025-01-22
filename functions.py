ee886c7597c1fb5dae990c85cd2139b86b81acf9
Kazeem Quadri <quadrikazeem01@gmail.com>
2025-01-22 14:58:14 +0000

    Add greeting functions to print user messages

---
+++ functions.py
@@ -0,0 +1,12 @@
+def say_hi():
+    print('Hello User')
+
+print('Top')
+say_hi()
+print('Bottom')
+
+
+def say_hi(name, age):
+    print('Hello ' + name + ', you are ' + age)
+
+say_hi('Mike', "35")
